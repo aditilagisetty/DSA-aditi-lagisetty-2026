fb06a9e6964cadf7ea4ceee39e3ecb284acd2b7b
aditilagisetty <lagisettyaditi@gmail.com>
2026-09-24 10:30:31 -0400

    added dijkstra tests to main

--- HW3/Main.kt
+++ HW3/Main.kt
@@ -14,4 +14,21 @@ fun main() {
     }
     println()
     println(heap.next())
+
+    // testing dijkstra
+    val graph: Graph<String> = AdjacencyMapGraph()
+    graph.addEdge("A", "B", 4.0)
+    graph.addEdge("A", "C", 1.0)
+    graph.addEdge("C", "B", 1.0)
+    graph.addEdge("B", "D", 1.0)
+    graph.addEdge("C", "D", 5.0)
+    graph.addEdge("Z", "Z", 0.0) // isolated vertex, unreachable from A
+
+    // expected shortest path A -> D is A, C, B, D with cost 3.0
+    val path = dijkstra(graph, "A", "D")
+    println("Shortest path A -> D: $path")
+
+    // Z has no edges to or from the rest of the graph so no path exists
+    val noPath = dijkstra(graph, "A", "Z")
+    println("Shortest path A -> Z (should be null): $noPath")
 }
